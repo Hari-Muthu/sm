09db03a64f7368854f0c9df56d2ef42a1617fbdc
HARIMUTHU-M <harimuthutce@gmail.com>
2022-09-10 13:38:59 +0530

    Co-authored-by: THOUFIQHUSSAIN-J <THOUFIQHUSSAIN-J@users.noreply.github.com> Co-authored-by: Sockalingam <Sockalingam29@users.noreply.github.com>
    Routes have been set and Login,Home,Detail components are created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/Home';
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!</h1>
+      <Router>
+          <Routes >
+            <Route index element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            {/* <Route path="/detail/:id" element={<Detail />} />*/}
+          </Routes >
+      </Router>
     </div>
   );
 }
